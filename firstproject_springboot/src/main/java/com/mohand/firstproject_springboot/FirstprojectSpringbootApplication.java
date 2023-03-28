8e266d21fa0158984b68cd315c90b953d4cb93b1
mohand alaidy <113944930+mohandalaidy171@users.noreply.github.com>
2023-03-29 02:30:51 +0300

    Add files via upload

---
+++ firstproject_springboot/src/main/java/com/mohand/firstproject_springboot/FirstprojectSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.mohand.firstproject_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectSpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FirstprojectSpringbootApplication.class, args);
+    }
+
+}
---
+++ firstproject_springboot/src/test/java/com/mohand/firstproject_springboot/FirstprojectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohand.firstproject_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
